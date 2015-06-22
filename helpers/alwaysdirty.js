676b8f92abbb4e4e055e1687cd1c00ca2fde3c92
Shad Storhaug <shad@shadstorhaug.com>
2015-06-22 19:35:52 +0700

    Fixed all issues identified by JSHint. Fixed formatting issues (using Visual Studio default formatting settings).

--- helpers/alwaysdirty.js
+++ helpers/alwaysdirty.js
@@ -14,13 +14,13 @@
         factory(jQuery);
     }
 }(function ($) {
-	// Create a new object, with an isDirty method
-	var alwaysDirty = {
-		isDirty : function(node){
-			// Perform dirty check on a given node (usually a form element)	
-			return true; 
-		}
-	}
-	// Push the new object onto the helpers array
-	$.DirtyForms.helpers.push(alwaysDirty);
+    // Create a new object, with an isDirty method
+    var alwaysDirty = {
+        isDirty: function (node) {
+            // Perform dirty check on a given node (usually a form element)	
+            return true;
+        }
+    };
+    // Push the new object onto the helpers array
+    $.DirtyForms.helpers.push(alwaysDirty);
 }));
