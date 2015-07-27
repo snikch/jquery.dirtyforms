d67ab6d55a18b3f3170bdd94be988c4ab175901a
MyGet Build Services <noreply@myget.org>
2015-07-27 22:35:18 +0000

    Release version 2.0.0-beta00002

--- dialogs/jquery-ui.js
+++ dialogs/jquery-ui.js
@@ -1,5 +1,5 @@
 /*!
-jQuery UI dialog module (for jQuery Dirty Forms) | v2.0.0-beta00001 | github.com/snikch/jquery.dirtyforms
+jQuery UI dialog module (for jQuery Dirty Forms) | v2.0.0-beta00002 | github.com/snikch/jquery.dirtyforms
 (c) 2015 Shad Storhaug
 License MIT
 */
